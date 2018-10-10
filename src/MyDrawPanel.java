3709282ebcdf397186ff02dbe2bfbe997a9236f9
Simi <nioasca@gmail.com>
2018-10-10 18:55:06 +0300

    Initial commit, not working>??

---
+++ src/MyDrawPanel.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+public class MyDrawPanel {
+    public void paintComponent(Graphics g) {
+
+
+
+        int red = (int) (Math.random()*255);
+        int green = (int) (Math.random()*255);
+        int blue = (int) (Math.random()*255);
+
+        Color randomColor = new Color(red, green, blue);
+        g.setColor(randomColor);
+        g.fillOval(70,70,100,100);
+
+    }
+}
